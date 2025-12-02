e5f753cc5645684147ddc1da0dd58231f19ccebd
muhammadShahroz69 <fayyazayan89@gmail.com>
2025-12-02 18:24:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello welcome to the condingal")
+print("hello welcome to the python learning")
+name="Obito"
+print("hello"+name)
+number = 67
+print("the number is",number)
+print(67)
+print("the sum of 6+7 is",6+7)
+import keyword
+print(keyword.kwlist)
